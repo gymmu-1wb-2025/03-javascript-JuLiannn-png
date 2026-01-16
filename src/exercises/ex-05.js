e03bd75df088f158ed14c646e95c22986820dd43
Julian Tiaden <e241824@edu.sbl.ch>
2026-01-16 12:36:59 +0000

    ex-05: Grundgerüst mit Text

--- src/exercises/ex-05.js
+++ src/exercises/ex-05.js
@@ -1,4 +1,14 @@
 const bodyheight = Number(process.argv);
+const talentScore = 243 - bodyheight;
 
-
-console.log(~`${243-bodyheight = }`)
+if (talentScore < 50) {
+	console.log("Du hast sehr viel Potenzial");
+} else if (50 <= talentScore && talentScore < 60) {
+	console.log("Du musst dringend an deiner Spungkraft arbeiten.");
+} else if (60 <= talentScore && talentScore < 70) {
+	console.log(
+		"Wenn du schnell bist, kannst du dir die Libero-Position überlegen.",
+	);
+} else if (70 <= talentScore) {
+	console.log("Du wirst im Volleyball nicht glücklich werden.");
+}
