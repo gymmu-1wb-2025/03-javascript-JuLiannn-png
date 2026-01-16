c447acf0b81ecf8c0463ad5811bf35d311527c8e
Julian Tiaden <e241824@edu.sbl.ch>
2026-01-16 13:47:02 +0000

    ex-07: Zusatz if mit exit

--- src/exercises/ex-07.js
+++ src/exercises/ex-07.js
@@ -2,6 +2,11 @@ const number = Number(process.argv[2]);
 const secondNumber = Number(process.argv[3]);
 const solution = number % secondNumber;
 
+if (secondNumber > number) {
+	console.log("Bist du eigentlich blööd....");
+	process.exit(1);
+}
+
 if (solution === 0) {
 	console.log("Ja ist teilbar");
 } else {
